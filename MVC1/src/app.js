7fa2d2ac411dbbe81168fb620c556879c4cfabe1
Tayane Pereira Ramos <tayanep.ramos@gmail.com>
2022-12-10 13:36:56 -0300

    feat(): adicao do swagger no projeto

--- MVC1/src/app.js
+++ MVC1/src/app.js
@@ -1,3 +1,5 @@
+const swaggerUi = require("swagger-ui-express");
+const swaggerFile = require("../swagger/swagger_output.json");
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
@@ -9,9 +11,15 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+
 mongoose.connect();
 
 app.use("/materialstore/material", materialRoute);
 app.use("/materialstore/user", autenticacaoRoute);
+app.use(
+  "/minha-rota-de-documentacao",
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerFile)
+);
 
 module.exports = app;
